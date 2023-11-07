49ed8bf0f5af21cd1a8c5c40a30a0000e31bd40e
Bonniellhwhite <bonniellhwhite@gmail.com>
2023-11-07 12:46:00 -0800

    Prep work, Instructions and helper files added

---
+++ assignment4/master.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <vector>
+#include <semaphore.h> 
+#include "mySmh.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc == 4) {
+        cout << "Master begins execution" << endl;
+
+        // Retrieving # of processes from argument
+        int num_processes = stoi(argv[1]);  // Use stoi to convert string to integer
+
+        // Retrieving Name from 3rd element of argument vector
+        string ms_name = argv[2];
+
+        // Retrieving Name from 4th element of argument vector 
+        string semaphore_name = argv[3];
+
+        // Creating shared memory segment (0666 means read-write permissions for owner, group, all)
+        int shm = shm_open(ms_name.c_str(), O_CREAT | O_RDWR, 0666);
+        ftruncate(shm, 4096);                                                    // Configure size of memory segment
+        CLASS* sharedData = static_cast<CLASS*>(mmap(0, sizeof(CLASS), PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0));  // Map shared memory segment
+        sharedData->index = 0;
+
+        cout << "Master created a shared memory segment named " << ms_name << endl;
+
+        cout << "Master created a semaphore named " << semaphore_name <<endl;
+
+
+
+        // Vector to store child process IDs
+        vector<pid_t> child_processes;
+
+        for (int i = 0; i < num_processes; i++) {       // for every process
+            pid_t child = fork();                       // Fork Parent aka Master 
+
+            if (child < 0) {                            // if id is negative, failed 
+                cerr << "Fork failed." << endl;
+                return 1;
+            } else if (child == 0) {                   // if success execute slave
+                // Child process
+                const char* child_args[] = {"./slave", to_string(i).c_str(), ms_name.c_str(), semaphore_name.c_str(),nullptr}; // creating an array of args, process number to cstr, and msname to cstr
+                execvp("./slave", const_cast<char* const*>(child_args)); // changing process image to slave file, executing those instructions 
+
+                // If execvp fails
+                perror("Exec failed");
+                return 1;
+            } else {
+                child_processes.push_back(child); // if everything was successful, add id to child process vector
+            }
+        }
+
+        cout << "Master created " << num_processes << "child processs to execute slave"<<endl;
+
+        cout << "Master waits for all child processes to terminate" << endl;
+        for (pid_t kid : child_processes) {
+            int status;
+            if (waitpid(kid, &status, 0) == -1) {
+                perror("Waitpid failed");
+                return 1;
+            }
+        }
+        cout << "Master received termination signals from all " << num_processes << " child processes" << endl;
+
+        // Print the updated content of shared memory
+        cout << "Updated content of shared memory segment after access by child processes:" << endl;
+        cout << " --- content of shared memory --- " << endl;
+        for (int i = 0; i < 10; ++i) {                      // for every byte from mySmh.h, print contents
+            cout << sharedData->response[i] << " ";
+        }
+        cout << endl;
+
+        // Remove shared memory segment
+        int removed_shm = shm_unlink(ms_name.c_str());
+        if (removed_shm == 0) {
+            cout << "Master removed shared memory segment, and is exiting" << endl;
+        } else {
+            perror("Error removing shared memory segment");
+            return 1;
+        }
+    } else {
+        cout << "Incorrect # of Arguments" << endl;
+        return 1;
+    }
+
+    return 0;
+}
---
+++ assignment4/slave.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+
+using namespace std;
+
+int main(int argc, char* argv[], char* argv[]) {
+    if (argc == 3) {
+        int child_number = stoi(argv[1]);
+
+        cout << "Slave begins execution" << endl;
+        cout << "I am child number " << child_number << ", received shared memory name " << argv[2] << "and" << argv[3] << endl;
+
+        // Open existing shared memory segment
+        int shm = shm_open(argv[2], O_RDWR, 0666);
+        if (shm == -1) {
+            perror("Failed to open shared memory segment");
+            return 1;
+        }
+
+        // Map shared memory into the address space of the process
+        void* ptr = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);
+        if (ptr == MAP_FAILED) {
+            cerr << "Failed to map shared memory" << endl;
+            close(shm);
+            return 1;
+        }
+
+        // Access shared memory
+        int* index = static_cast<int*>(ptr);
+        int child_slot = (*index)++;  // Get the next available slot
+        int luckyNum;
+        cout << "Child number" << child_number << ": What is my lucky number?"<< endl;
+        cout << "Enter Number:";
+        cin >> luckyNum;
+
+        // Write child number to the shared memory
+        int* child_number_ptr = static_cast<int*>(ptr) + 1 + child_slot;
+        *child_number_ptr = child_number;
+        //sprintf(ptr,"%s",message0);
+
+        // Print the message
+        cout << "I have written my child number to slot " << index << " and my lucky number to " << index+1 << ", and updated index to" << index+2 << endl; 
+
+        // Unmap shared memory
+        munmap(ptr, sizeof(int));
+
+        // Close shared memory
+        close(shm);
+
+        cout << "Child " << child_number << " closed access to shared memory and terminates." << endl;
+    } else {
+        cout << "Usage: ./slave <child_number> <ms_name>" << endl;
+        return 1;
+    }
+
+    return 0;
+}
